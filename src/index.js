ba1f91c5646cc93b04ed42f342d3b741761bead4
jeganumapathy <u.jegan@yahoo.co.in>
2024-05-25 10:56:38 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,13 +10,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4EtYfn1CTtYPVgNB_bgwXrdnZPbmYS7c",
-  authDomain: "stockmarkettools-6612a.firebaseapp.com",
-  projectId: "stockmarkettools-6612a",
-  storageBucket: "stockmarkettools-6612a.appspot.com",
-  messagingSenderId: "229054118439",
-  appId: "1:229054118439:web:c87e8154fef96d9f73ac02",
-  measurementId: "G-RX1GBHPKYC"
 };
 
 // Initialize Firebase
