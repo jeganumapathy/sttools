3993519400dccc185b7a40498f87e72bacba5a79
jeganumapathy <u.jegan@yahoo.co.in>
2024-05-25 10:07:44 +0530

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 SIMPLE TOOLS
+
+Additional Notes
+
+ON Windows
+https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-//https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth} from "firebase/auth";
