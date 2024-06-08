03f4f5d13c19378490fa5ef6123b5d325016a2b7
jeganumapathy <u.jegan@yahoo.co.in>
2024-06-08 17:15:51 +0530

    updated to latest

--- src/menu-items/page.jsx
+++ src/menu-items/page.jsx
@@ -29,14 +29,6 @@ const pages = {
       url: '/register',
       icon: icons.ProfileOutlined,
       target: true
-    },
-    {
-      id: 'register1',
-      title: 'Register',
-      type: 'item',
-      url: '/register',
-      icon: icons.ProfileOutlined,
-      target: true
     }
   ]
 };
